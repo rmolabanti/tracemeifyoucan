2908194a538efe5ee12af8ebfe6d7e39565c1cb9
Raghu Babu Molabanti <rmolabanti@reputation.com>
2021-02-19 20:05:56 +0530

    publisher

---
+++ publisher/src/main/java/com/tracemeifyoucan/publisher/Publisher.java
@@ -0,0 +1,12 @@
+package com.tracemeifyoucan.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Publisher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Publisher.class, args);
+    }
+}
