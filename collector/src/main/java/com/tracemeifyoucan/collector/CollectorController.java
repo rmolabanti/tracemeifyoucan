8bdd6fde422f7a3c0032d3f3b4d17511d25a4621
Bharath Kumar Adhikari <badhikari@reputation.com>
2021-02-20 22:34:30 +0530

    Added redis communication betweeen governer and collector

--- collector/src/main/java/com/tracemeifyoucan/collector/CollectorController.java
+++ collector/src/main/java/com/tracemeifyoucan/collector/CollectorController.java
@@ -1,9 +1,15 @@
 package com.tracemeifyoucan.collector;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
 
 @RestController
+@EnableScheduling
 public class CollectorController {
 
     @GetMapping("/collect")
@@ -11,5 +17,19 @@ public class CollectorController {
         Entity entity = new Entity();
         entity.setName("name");
         return entity;
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void redispush(){
+        Jedis jedis = new Jedis("localhost");
+        jedis.lpush("Q1","TEST");
+    }
+
+
+    @Scheduled(fixedDelay = 1000)
+    public  void redispop(){
+        Jedis jedis = new Jedis("localhost");
+        List<String> messages = jedis.blpop(0,"Q2");
+        System.out.println(messages.get(1));
     }
 }
--- governor/src/main/java/com/tracemeifyoucan/governor/GovernorController.java
+++ governor/src/main/java/com/tracemeifyoucan/governor/GovernorController.java
@@ -1,14 +1,21 @@
 package com.tracemeifyoucan.governor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import redis.clients.jedis.Jedis;
+
 
 @RestController
+@EnableScheduling
 public class GovernorController {
 
 
@@ -23,4 +30,17 @@ public class GovernorController {
         Thread.sleep(1000);
         return 1;
     }
+
+    @Scheduled(fixedDelay = 1000)
+    public  void redispush() throws InterruptedException {
+        Jedis jedis = new Jedis("localhost");
+        List<String> messages = jedis.blpop(0,"Q1");
+        System.out.println(messages.get(1));
+        System.out.println("in governer");
+        Thread.sleep(1000);
+        jedis.rpush("Q2",messages.get(1));
+    }
+
+
+
 }
