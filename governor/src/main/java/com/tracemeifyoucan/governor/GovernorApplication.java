efbfdc5f24797549fd3a39c0e2b7023c25a4106d
Raghu Babu Molabanti <rmolabanti@reputation.com>
2021-02-20 18:56:08 +0530

    replaced governor

---
+++ governor/src/main/java/com/tracemeifyoucan/governor/GovernorApplication.java
@@ -0,0 +1,13 @@
+package com.tracemeifyoucan.governor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernorApplication.class, args);
+	}
+
+}
---
+++ governor/src/test/java/com/tracemeifyoucan/governor/GovernorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracemeifyoucan.governor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
